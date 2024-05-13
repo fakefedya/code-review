7d0136646084489d30ef9ccbff97fbb7edbe53eb
Fedya <polevoy@ligat.su>
2024-05-13 13:35:02 +0300

    6-driver

---
+++ 4-navigator/app.js
@@ -0,0 +1,23 @@
+// const UserAnswer = prompt('Сколько будет 7 + или - 15?')
+// switch (true) {
+// 	case Number(UserAnswer) === 22:
+// 	case Number(UserAnswer) === -8:
+// 	case UserAnswer === 'Я не робот':
+// 		console.log('Успех')
+// 		break
+// 	default:
+// 		console.log('Вы робот!')
+// }
+
+const a = 5
+switch (a) {
+	case '5':
+		console.log('1')
+	case 5:
+		console.log('2')
+		break
+	case 4:
+		console.log('2')
+	case 422:
+		console.log('2123')
+}
