3dd4e7489eaf84ebebb214e84d620988161d7a13
Fedya <polevoy@ligat.su>
2024-05-13 13:58:35 +0300

    Убрался в песочнице 4-navigator

--- 4-navigator/app.js
+++ 4-navigator/app.js
@@ -1,23 +1,12 @@
-// const UserAnswer = prompt('Сколько будет 7 + или - 15?')
-// switch (true) {
-// 	case Number(UserAnswer) === 22:
-// 	case Number(UserAnswer) === -8:
-// 	case UserAnswer === 'Я не робот':
-// 		console.log('Успех')
-// 		break
-// 	default:
-// 		console.log('Вы робот!')
-// }
+const adressLat = 1,
+	adressLong = 2,
+	positionLat = -10,
+	positionLong = 2
 
-const a = 5
-switch (a) {
-	case '5':
-		console.log('1')
-	case 5:
-		console.log('2')
-		break
-	case 4:
-		console.log('2')
-	case 422:
-		console.log('2123')
-}
+/*
+Вычисление расстояния между двумя точками на плоскости
+Формула: AB = √((xb - xa)2 + (yb - ya)2)
+*/
+console.log(
+	Math.sqrt((positionLat - adressLat) ** 2 + (positionLong - adressLong) ** 2)
+)
