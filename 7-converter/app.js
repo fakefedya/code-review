b01bf3e14caf86631f22a62ef7f5870fb66f9fb1
Fedya <polevoy@ligat.su>
2024-05-15 14:35:53 +0300

    Фикс конвертера

--- 7-converter/app.js
+++ 7-converter/app.js
@@ -1,18 +1,15 @@
 const currencyConverter = (currencySum, currencyOriginal, currencyTarget) => {
+	const RUB = 1
+	const USD = 91.34
+	const EUR = 98.88
 	switch (currencyOriginal.toUpperCase()) {
 		case 'RUB':
-			const usdToRub = 91.34
-			const eurToRub = 98.88
-			const kztToRub = 0.21
 			switch (currencyTarget.toUpperCase()) {
 				case 'USD':
-					return (currencySum / usdToRub).toFixed(2)
+					return (currencySum / USD).toFixed(2)
 					break
 				case 'EUR':
-					return (currencySum / eurToRub).toFixed(2)
-					break
-				case 'KZT':
-					return (currencySum / kztToRub).toFixed(2)
+					return (currencySum / EUR).toFixed(2)
 					break
 				default:
 					console.log('Целевая валюта не найдена!')
@@ -20,18 +17,25 @@ const currencyConverter = (currencySum, currencyOriginal, currencyTarget) => {
 			}
 			break
 		case 'USD':
-			const rubToUsd = 0.011
-			const eurToUsd = 1.08
-			const kztToUsd = 0.0023
 			switch (currencyTarget.toUpperCase()) {
 				case 'RUB':
-					return (currencySum / rubToUsd).toFixed(2)
+					return (currencySum * USD).toFixed(2)
 					break
 				case 'EUR':
-					return (currencySum / eurToUsd).toFixed(2)
+					return ((currencySum * EUR) / USD).toFixed(2)
 					break
-				case 'KZT':
-					return (currencySum / kztToUsd).toFixed(2)
+				default:
+					console.log('Целевая валюта не найдена!')
+					return 0
+			}
+			break
+		case 'EUR':
+			switch (currencyTarget.toUpperCase()) {
+				case 'RUB':
+					return (currencySum * EUR).toFixed(2)
+					break
+				case 'USD':
+					return ((currencySum * USD) / EUR).toFixed(2)
 					break
 				default:
 					console.log('Целевая валюта не найдена!')
@@ -45,5 +49,5 @@ const currencyConverter = (currencySum, currencyOriginal, currencyTarget) => {
 }
 console.log(currencyConverter(1, 'USD', 'RUB'))
 console.log(currencyConverter(100, 'RUB', 'USD'))
-console.log(currencyConverter(100, 'RUB', 'RMB'))
-console.log(currencyConverter(100, 'RMB', 'USD'))
+console.log(currencyConverter(1, 'USD', 'EUR'))
+console.log(currencyConverter(1, 'EUR', 'USD'))
