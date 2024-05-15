305e3facb41e641a28279aab646de9dd5b98f43d
Fedya <polevoy@ligat.su>
2024-05-15 14:19:58 +0300

    Фикс конвертера, но есть вопросы, правильно ли понял ревью

--- 7-converter/app.js
+++ 7-converter/app.js
@@ -1,47 +1,49 @@
-const CURRENCY_TO_SOMETHING = (currencyOriginal, currencyTarget) => {
-	switch (true) {
-		case currencyOriginal.toLowerCase() === 'rub':
-			switch (currencyTarget.toLowerCase()) {
-				case 'usd':
-					return 0.011
-				case 'kzt':
-					return 4.82
-				case 'rmb':
-					return 0.079
+const currencyConverter = (currencySum, currencyOriginal, currencyTarget) => {
+	switch (currencyOriginal.toUpperCase()) {
+		case 'RUB':
+			const usdToRub = 91.34
+			const eurToRub = 98.88
+			const kztToRub = 0.21
+			switch (currencyTarget.toUpperCase()) {
+				case 'USD':
+					return (currencySum / usdToRub).toFixed(2)
+					break
+				case 'EUR':
+					return (currencySum / eurToRub).toFixed(2)
+					break
+				case 'KZT':
+					return (currencySum / kztToRub).toFixed(2)
+					break
 				default:
+					console.log('Целевая валюта не найдена!')
 					return 0
 			}
 			break
-		case currencyOriginal.toLowerCase() === 'usd':
-			switch (currencyTarget.toLowerCase()) {
-				case 'rub':
-					return 91.631
-				case 'kzt':
-					return 441.48
-				case 'rmb':
-					return 7.23
+		case 'USD':
+			const rubToUsd = 0.011
+			const eurToUsd = 1.08
+			const kztToUsd = 0.0023
+			switch (currencyTarget.toUpperCase()) {
+				case 'RUB':
+					return (currencySum / rubToUsd).toFixed(2)
+					break
+				case 'EUR':
+					return (currencySum / eurToUsd).toFixed(2)
+					break
+				case 'KZT':
+					return (currencySum / kztToUsd).toFixed(2)
+					break
 				default:
-					return 0
-			}
-			break
-		case currencyOriginal.toLowerCase() === 'kzt':
-			switch (currencyTarget.toLowerCase()) {
-				case 'rub':
-					return 0.21
-				case 'usd':
-					return 0.0023
-				case 'rmb':
-					return 0.016
-				default:
+					console.log('Целевая валюта не найдена!')
 					return 0
 			}
 			break
 		default:
+			console.log('Курс неизвестен!')
 			return 0
 	}
 }
-
-const convertedCurrency = (currencyOriginal, currencyTarget, moneyCount) =>
-	CURRENCY_TO_SOMETHING(currencyOriginal, currencyTarget) * moneyCount
-
-console.log(convertedCurrency('usd', 'rub', 1).toLocaleString('ru'))
+console.log(currencyConverter(1, 'USD', 'RUB'))
+console.log(currencyConverter(100, 'RUB', 'USD'))
+console.log(currencyConverter(100, 'RUB', 'RMB'))
+console.log(currencyConverter(100, 'RMB', 'USD'))
