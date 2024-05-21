1af78426116d2a942932ab56e233abe1597f8f8b
Fedya <polevoy@ligat.su>
2024-05-21 17:06:03 +0300

    11 fixed?

--- 11-date-array/app.js
+++ 11-date-array/app.js
@@ -2,6 +2,7 @@ const arr = [
 	'10-02-2022',
 	'40-02-2022',
 	'12-13-2022',
+	'31-02-2022',
 	'12/13/2025',
 	'тест',
 	'те-те-тест',
@@ -10,32 +11,66 @@ const arr = [
 	'00/13/2022',
 	'11/14/2022',
 	'41/12/2023',
+	'1s2-3-203q',
 ]
-const validDateCheck = (arr) => {
-	const validDateArr = []
-	for (let arrElement of arr) {
+
+const getDates = (arr) => {
+	if (arr.length !== 0) {
+		return arr
+	}
+	console.log('Исходный массив пустой!')
+	return
+}
+
+const datesToOneFormat = (fn) => {
+	const formatedArr = []
+	for (let element of fn) {
 		switch (true) {
-			case arrElement.length === 10 && arrElement.indexOf('-') > -1:
-				const [dayRu, monthRu] = arrElement.split('-')
-				dayRu > 0 && dayRu <= 31 && monthRu > 0 && monthRu <= 12
-					? validDateArr.push(arrElement)
-					: false
+			case element.indexOf('-') !== -1:
+				formatedArr.push(element)
 				break
-			case arrElement.length === 10 && arrElement.indexOf('/') > -1:
-				const tmpEl = [
-					arrElement.split('/')[1],
-					arrElement.split('/')[0],
-					arrElement.split('/')[2],
-				]
-				arrElement.concat(tmpEl)
-				const [monthUs, dayUs] = arrElement.split('/')
-				dayUs > 0 && dayUs <= 31 && monthUs > 0 && monthUs <= 12
-					? validDateArr.push(arrElement.replace(/[ / ]/g, '-'))
-					: false
+			case element.indexOf('/') !== -1:
+				const [month, day, year] = element.split('/')
+				element = [day, month, year]
+				element = element.join('-')
+				formatedArr.push(element)
 				break
 			default:
 		}
 	}
-	return validDateArr
+	console.log('Преобразуем даты в один формат:', formatedArr)
+	return formatedArr
 }
-console.log(validDateCheck(arr))
+
+const validateDates = (fn) => {
+	const validatedArr = []
+	for (const element of fn) {
+		const [day, month, year] = element.split('-')
+		switch (true) {
+			case element.length === 10 &&
+				isNaN(day) === false &&
+				isNaN(month) === false &&
+				isNaN(year) === false:
+				switch (true) {
+					case month === '02' && day > 0 && day <= 28 && year > 0:
+						validatedArr.push(element)
+						break
+					case month > 0 &&
+						month <= 12 &&
+						month !== '02' &&
+						day > 0 &&
+						day <= 31 &&
+						year > 0:
+						validatedArr.push(element)
+						break
+					default:
+				}
+				break
+			default:
+		}
+	}
+	console.log('Валидируем даты (без учета високосного года):', validatedArr)
+	return validatedArr
+}
+
+validateDates(datesToOneFormat(getDates(arr)))
