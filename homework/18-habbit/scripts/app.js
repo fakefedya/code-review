e41ca1b7775cda3d25367492614db1459d65e123
Фёдор Полевой <polevoy@ligat.su>
2024-06-21 16:07:23 +0300

    tmp

--- homework/18-habbit/scripts/app.js
+++ homework/18-habbit/scripts/app.js
@@ -3,6 +3,11 @@
 let habbits = []
 const HABBIT_KEY = 'HABBIT_KEY'
 
+/* page */
+const page = {
+	menu: document.querySelector('.menu-list'),
+}
+
 /* utils */
 
 function loadData() {
@@ -16,6 +21,47 @@ function loadData() {
 function saveData() {
 	localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits))
 }
+
+/* render */
+
+function reRenderMenu(activeHabbit) {
+	if (!activeHabbit) {
+		return
+	}
+	document.querySelector('.menu-list').innerHTML = ''
+	for (const habbit of habbits) {
+		const existed = document.querySelector(`[menu-habbit-id="${habbit.id}]"`)
+		if (!existed) {
+			const element = document.createElement('button')
+			element.setAttribute('menu-habbit-id', habbit.id)
+			element.classList.add('menu-item')
+			element.addEventListener('click', () => reRender(habbit.id))
+			element.innerHTML = `<img src="./images/icon-${habbit.icon}.svg" alt="${habbit.name}"/>`
+			if (activeHabbit.id === habbit.id) {
+				element.classList.add('menu-item-active')
+			}
+			page.menu.appendChild(element)
+			continue
+		}
+		if (activeHabbit.id === habbit.id) {
+			existed.classList.add('menu-item-active')
+		} else {
+			existed.classList.remove('menu-item-active')
+		}
+	}
+}
+
+function renderHead(activeHabbit) {}
+
+function reRender(activeHabbitId) {
+	const activeHabbit = habbits.find((habbit) => habbit.id === activeHabbitId)
+	reRenderMenu(activeHabbit)
+	renderHead(activeHabbit)
+}
+
+/* init */
+
 ;(() => {
 	loadData()
+	reRender(habbits[0].id)
 })()
