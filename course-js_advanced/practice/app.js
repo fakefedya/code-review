d0d3442d42a5ef0014eec4da3b8f8a0ed961cd45
Фёдор Полевой <polevoy@ligat.su>
2024-07-09 16:04:38 +0300

    bigInt & Intl

--- course-js_advanced/practice/app.js
+++ course-js_advanced/practice/app.js
@@ -1,13 +1,48 @@
 'use strict'
 
-/*
-	Написать функцию, которая принимает min и max
-	и возвращает случайно целое число между ними, включая их
-*/
+console.log(Number.MAX_SAFE_INTEGER)
+const max = Number.MAX_SAFE_INTEGER
+const min = Number.MIN_SAFE_INTEGER
+console.log(max, min)
+console.log(max + 1)
+console.log(max + 4)
+console.log(max + 3)
 
-function random(min, max) {
-	const res = Math.floor(Math.random() * (max - min + 1) + min)
-	return res
+/* BigInt */
+
+console.log(4561437651243567214375124312434n)
+console.log(BigInt(4561437651243567214375124312434))
+console.log(10n * BigInt(10))
+console.log(10n / BigInt(3))
+
+/* Интернационализация чисел */
+
+const options1 = {
+	style: 'currency',
+	currency: 'RUB',
+	useGrouping: false,
 }
 
-console.log(random(1, 10))
+const options2 = {
+	style: 'currency',
+	currency: 'USD',
+}
+
+const options3 = {
+	style: 'decimal',
+}
+
+const options4 = {
+	style: 'percent',
+}
+
+const options5 = {
+	style: 'unit',
+	unit: 'celsius',
+}
+
+console.log(new Intl.NumberFormat('ru-RU', options1).format(23000))
+console.log(new Intl.NumberFormat('en-US', options2).format(23000))
+console.log(new Intl.NumberFormat('ru-RU', options3).format(10000))
+console.log(new Intl.NumberFormat('ru-RU', options4).format(0.1))
+console.log(new Intl.NumberFormat('ru-RU', options5).format(25))
