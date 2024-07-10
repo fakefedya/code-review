4d1919b9ad0c138520b5b5dc6cea09ccb1607b89
Фёдор Полевой <polevoy@ligat.su>
2024-07-10 10:16:41 +0300

    2-dice (upd.)

--- course-js_advanced/2-dice/app.js
+++ course-js_advanced/2-dice/app.js
@@ -5,12 +5,6 @@ d4, d6, d8, d10, d12, d16, d20
 Вернуть: случайное целое число в интервале с включенными границами
 */
 
-/*
-Идея: принимать наименование многогранника и разбивать его имя на 'd' и число + сравнивать его имя с массивом наименований многогранников, чтобы проверять корректность введенного наименования, после чего смотреть его самую высокую по значению грань (max) и минимальную грань = 1 (всегда) (min), после:
-	const res = Math.floor(Math.random() * (max - min + 1) + min)
-	return res
-*/
-
 function dice(typeOfDice) {
 	const MIN_DICE_VAL = 1
 
@@ -26,16 +20,16 @@ function dice(typeOfDice) {
 
 	const selectedTypeOfDice = diceTypeArr.find((d) => d.diceName === typeOfDice)
 	if (!selectedTypeOfDice) {
-		console.log(`Введен некорректный тип многогранника: "${typeOfDice}"`)
-		return
+		return `Введен некорректный тип многогранника: "${typeOfDice}"`
 	}
 	const maxDiceVal = selectedTypeOfDice.diceName.split('d').pop()
-	const res = Math.floor(
+	return Math.floor(
 		Math.random() * (maxDiceVal - MIN_DICE_VAL + 1) + MIN_DICE_VAL
 	)
-	return res
 }
 
 console.log(dice('d4'))
 console.log(dice('d6'))
 console.log(dice('d20'))
+console.log(dice('d2'))
+console.log(dice('d10'))
