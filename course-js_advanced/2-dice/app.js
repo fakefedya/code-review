6b761cd8e46f15694bb5ea4b7c4184292bf7944c
Фёдор Полевой <polevoy@ligat.su>
2024-07-11 09:17:03 +0300

    Небольшие улучшения

--- course-js_advanced/2-dice/app.js
+++ course-js_advanced/2-dice/app.js
@@ -7,22 +7,15 @@ d4, d6, d8, d10, d12, d16, d20
 
 function dice(typeOfDice) {
 	const MIN_DICE_VAL = 1
+	const allowedTypesOfDice = ['d4', 'd6', 'd8', 'd10', 'd12', 'd16', 'd20']
+	const selectedTypeOfDice = allowedTypesOfDice.find((d) => d === typeOfDice)
 
-	diceTypeArr = [
-		{ diceName: 'd4' },
-		{ diceName: 'd6' },
-		{ diceName: 'd8' },
-		{ diceName: 'd10' },
-		{ diceName: 'd12' },
-		{ diceName: 'd16' },
-		{ diceName: 'd20' },
-	]
-
-	const selectedTypeOfDice = diceTypeArr.find((d) => d.diceName === typeOfDice)
 	if (!selectedTypeOfDice) {
 		return `Введен некорректный тип многогранника: "${typeOfDice}"`
 	}
-	const maxDiceVal = selectedTypeOfDice.diceName.split('d').pop()
+
+	const maxDiceVal = selectedTypeOfDice.replace('d', '')
+
 	return Math.floor(
 		Math.random() * (maxDiceVal - MIN_DICE_VAL + 1) + MIN_DICE_VAL
 	)
