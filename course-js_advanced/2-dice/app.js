f03ddb93a5b4b6efeb8eb94dd845609409a77a0a
Фёдор Полевой <polevoy@ligat.su>
2024-07-14 20:32:53 +0300

    Объект вместо массива

--- course-js_advanced/2-dice/app.js
+++ course-js_advanced/2-dice/app.js
@@ -7,17 +7,22 @@ d4, d6, d8, d10, d12, d16, d20
 
 function dice(typeOfDice) {
 	const MIN_DICE_VAL = 1
-	const allowedTypesOfDice = ['d4', 'd6', 'd8', 'd10', 'd12', 'd16', 'd20']
-	const selectedTypeOfDice = allowedTypesOfDice.find((d) => d === typeOfDice)
-
-	if (!selectedTypeOfDice) {
+	const allowedTypesOfDice = {
+		d4: '4',
+		d6: '6',
+		d8: '8',
+		d10: '10',
+		d12: '12',
+		d16: '16',
+		d20: '20',
+	}
+	if (!allowedTypesOfDice[typeOfDice]) {
 		return `Введен некорректный тип многогранника: "${typeOfDice}"`
 	}
-
-	const maxDiceVal = selectedTypeOfDice.replace('d', '')
-
 	return Math.floor(
-		Math.random() * (maxDiceVal - MIN_DICE_VAL + 1) + MIN_DICE_VAL
+		Math.random() *
+			(Number(allowedTypesOfDice[typeOfDice]) - MIN_DICE_VAL + 1) +
+			MIN_DICE_VAL
 	)
 }
 
