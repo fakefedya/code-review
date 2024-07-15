6ced6c781407932e866adbc60eb621baf4b8b519
Фёдор Полевой <polevoy@ligat.su>
2024-07-15 18:51:15 +0300

    Таймер с учетом оконачений существительных

--- course-js_advanced/4-timer/app.js
+++ course-js_advanced/4-timer/app.js
@@ -1,40 +1,77 @@
+/*
+Сделать приложение, которое в браузере выводит таймер реального времени, который показывает 
+сколько осталось до нового года в формате:
+ ХХ месяцев, ХХ дней, ХХ часов, ХХ минут, ХХ секунд
+
+	Учесть окончания существительных?
+*/
+
 'use strict'
 
-// const page = {
-// 	countDiv: document.querySelector('.count'),
-// }
-
-function newYearTimeRemaining() {
-	const NEW_YEAR_DATE = '2024-12-31'
-	const dateToday = new Date()
-	// console.log(dateToday, new Date(NEW_YEAR_DATE))
-	const timeRemaining = new Date(NEW_YEAR_DATE) - dateToday
-	console.log(timeRemaining)
-	const secsRemaining = timeRemaining / 1000
-	const minsRemaining = secsRemaining / 60
-	const hoursRemaining = minsRemaining / 60
-	const daysRemaining = hoursRemaining / 24
-	const monthsRemaining = daysRemaining / 30
-	const weeksRemaining = daysRemaining / 7
-	console.log(
-		secsRemaining,
-		minsRemaining,
-		hoursRemaining,
-		daysRemaining,
-		weeksRemaining,
-		monthsRemaining
-	)
-	// console.log(
-	// 	new Intl.DateTimeFormat('ru-RU', {
-	// 		second: 'numeric',
-	// 		hour: 'numeric',
-	// 		minute: 'numeric',
-	// 		year: 'numeric',
-	// 		month: 'numeric',
-	// 		day: 'numeric',
-	// 	}).format(timeRemaining)
-	// )
+const page = {
+	countDiv: document.querySelector('.count'),
 }
-newYearTimeRemaining()
 
-// console.log(navigator.language)
+function counterNewYear() {
+	const countDownTimer = setInterval(() => {
+		calcTimeAndRenderCount()
+	}, 1000)
+	setTimeout(() => {
+		clearInterval(countDownTimer)
+		page.countDiv.innerHTML = `Ура! С Новым ${
+			new Date().getFullYear() + 1
+		} годом!`
+	}, new Date(`1 Jan ${new Date().getFullYear() + 1} 00:00:00`) - new Date())
+}
+
+function createLabel(numberOf, typeOfLabel) {
+	const LOCALE = 'ru-RU'
+
+	const pluralOptions = {
+		type: 'cardinal',
+	}
+
+	const labelsArr = [
+		['месяц', 'месяца', 'месяцев'],
+		['день', 'дня', 'дней'],
+		['час', 'часа', 'часов'],
+		['минута', 'минуты', 'минут'],
+		['секунда', 'секунды', 'секунд'],
+	]
+
+	const pluralMap = new Map()
+	pluralMap.set('one', 0)
+	pluralMap.set('few', 1)
+	pluralMap.set('many', 2)
+
+	const plural = new Intl.PluralRules(LOCALE, pluralOptions).select(numberOf)
+	const mappedPlural = pluralMap.get(plural)
+
+	return labelsArr[typeOfLabel][mappedPlural]
+}
+
+function calcTimeAndRenderCount() {
+	const now = new Date()
+	const newYear = new Date(`1 Jan ${now.getFullYear() + 1} 00:00:00`)
+	const datesDiff = newYear - now
+	const messageArr = []
+
+	const timeObj = {
+		months: Math.floor(datesDiff / 1000 / 60 / 60 / 24 / 30.425),
+		days: Math.floor(datesDiff / 1000 / 60 / 60 / 24),
+		hours: Math.floor((datesDiff / 1000 / 60 / 60) % 24),
+		minutes: Math.floor((datesDiff / 1000 / 60) % 60),
+		seconds: Math.floor((datesDiff / 1000) % 60),
+	}
+
+	Object.values(timeObj).forEach((numberOf, typeOfLabel) => {
+		if (numberOf !== 0) {
+			messageArr.push(`${numberOf} ${createLabel(numberOf, typeOfLabel)}`)
+		}
+	})
+	return (page.countDiv.innerHTML = messageArr.join(', ').replaceAll('-', ''))
+}
+
+;(() => {
+	counterNewYear()
+})()
