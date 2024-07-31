d0b5fc45012626a1119100ff8c67518c94b25f8b
Фёдор Полевой <polevoy@ligat.su>
2024-07-31 20:35:35 +0300

    8.6

--- course-js_advanced/practice/app.js
+++ course-js_advanced/practice/app.js
@@ -1,43 +1,22 @@
 'use strict'
 
-class User {
-	#login
-	#cryptoPassword
-
-	constructor(login, password) {
-		this.#login = login
-		this.#password = password
-	}
-
-	set #password(password) {
-		this.#cryptoPassword = password.split('').reverse().join('')
-	}
-
-	get #password() {
-		return this.#cryptoPassword.split('').reverse().join('')
-	}
-
-	get login() {
-		return this.#login
-	}
-
-	checkPassword(pass) {
-		return this.#password === pass
-	}
-
-	changePassword(oldPass, newPass) {
-		if (!this.checkPassword(oldPass)) {
-			return false
-		}
-		this.#password = newPass
-		return true
-	}
+const User = {
+	init(email, password) {
+		this.email = email
+		this.password = password
+	},
+	log() {
+		console.log('Log')
+	},
 }
 
-const user = new User('a@a.ru', '1234')
+const user = Object.create(User)
+console.log(user.__proto__ == User)
+user.log()
+// user.email = 'a@a.ru'
+// user.password = '123'
+user.init('a@a.ru', '123')
 console.log(user)
-console.log(user.login)
-console.log(user.checkPassword('1234'))
-console.log(user.changePassword('11234', '5678'))
-console.log(user.changePassword('1234', '5678'))
-console.log(user)
+
+const admin = Object.create(user)
+console.log(admin)
