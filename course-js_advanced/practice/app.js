b9b6fea7273c226165abca35a58244d032cc5804
Фёдор Полевой <polevoy@ligat.su>
2024-07-31 15:24:51 +0300

    8.3

--- course-js_advanced/practice/app.js
+++ course-js_advanced/practice/app.js
@@ -1,42 +1,17 @@
 'use strict'
 
-const task = {
-	title: 'Task1',
-	dueTo: new Date('2023/01/01'),
+class Test {
+	static a = 1
 
-	get isOverDue() {
-		return this.dueTo < new Date()
-	},
-
-	set isOverDue(isOver) {
-		if (!isOver) {
-			this.dueTo = new Date()
-		}
-	},
-}
-
-console.log(task.isOverDue)
-task.isOverDue = false
-console.log(task)
-
-class Task {
-	constructor(title, dueDate) {
-		this.title = title
-		this.dueDate = dueDate
+	static hello() {
+		console.log('Hello!')
 	}
 
-	get isOverdue() {
-		return this.dueDate < new Date()
-	}
-
-	set dueDate(date) {
-		if (date < new Date()) {
-			return
-		}
-		this._dueDate = date
+	static {
+		let b = 5
+		this.a = 5
 	}
 }
 
-const newTask = new Task('Task2', new Date())
-console.log(newTask.isOverdue)
-console.log(newTask)
+Test.hello()
+console.log(Test.a)
