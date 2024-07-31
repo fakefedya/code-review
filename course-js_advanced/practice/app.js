d79f953d6e2e301b88ea1f371c7cf4826b54aa90
Фёдор Полевой <polevoy@ligat.su>
2024-07-31 16:16:50 +0300

    8.5

--- course-js_advanced/practice/app.js
+++ course-js_advanced/practice/app.js
@@ -1,19 +1,43 @@
 'use strict'
 
-class Car {
-	#vin = 6
-	speed
+class User {
+	#login
+	#cryptoPassword
 
-	constructor() {}
-
-	#changeVin() {
-		console.log('Changed!')
+	constructor(login, password) {
+		this.#login = login
+		this.#password = password
 	}
 
-	test() {
-		this.#changeVin()
+	set #password(password) {
+		this.#cryptoPassword = password.split('').reverse().join('')
+	}
+
+	get #password() {
+		return this.#cryptoPassword.split('').reverse().join('')
+	}
+
+	get login() {
+		return this.#login
+	}
+
+	checkPassword(pass) {
+		return this.#password === pass
+	}
+
+	changePassword(oldPass, newPass) {
+		if (!this.checkPassword(oldPass)) {
+			return false
+		}
+		this.#password = newPass
+		return true
 	}
 }
 
-const car = new Car()
-car.speed
+const user = new User('a@a.ru', '1234')
+console.log(user)
+console.log(user.login)
+console.log(user.checkPassword('1234'))
+console.log(user.changePassword('11234', '5678'))
+console.log(user.changePassword('1234', '5678'))
+console.log(user)
