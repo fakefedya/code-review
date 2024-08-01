24cfeba910f5a36fce62b587e5721d2517b384c7
Фёдор Полевой <polevoy@ligat.su>
2024-08-01 09:38:41 +0300

    9.2

--- course-js_advanced/practice/app.js
+++ course-js_advanced/practice/app.js
@@ -1,41 +1,27 @@
 'use strict'
 
-/*
-	Фильм
-	- Название
-	- Режиссер
-	- Рейтинг
-	- Длительность
-	- Страна производства
-	- Актеры
-	- Трейлер
-	...
-*/
-
-class Film {
-	#name
-	#author
-	rating
-	#length
-
-	constructor(name, author, length) {
-		this.#name = name
-		this.#author = author
-		this.#length = length
-	}
-
-	get name() {
-		return this.#name
-	}
-
-	get author() {
-		return this.#author
-	}
-
-	get length() {
-		return this.#length
-	}
+const Book = function (title, author) {
+	this.title = title
+	this.author = author
 }
 
-const film = new Film('Avatar', 'Cameron', 240)
-console.log(film)
+Book.prototype.buy = function () {
+	console.log('Buy')
+}
+
+const AudioBook = function (title, author, lenMin) {
+	Book.call(this, title, author)
+	this.lenMin = lenMin
+}
+
+AudioBook.prototype = Object.create(Book.prototype)
+AudioBook.prototype.constructor = AudioBook
+
+AudioBook.prototype.log = function () {
+	console.log(`${this.title} - ${this.lenMin}`)
+}
+
+const book = new AudioBook('Lord Of The Rings', 'Tolkien', 20 * 60)
+
+book.log()
+book.buy()
