44fd06cb73e1bdcae9f9b27194b747fda482ccd0
Фёдор Полевой <polevoy@ligat.su>
2024-08-12 15:18:59 +0300

    10.2

--- course-js_advanced/practice/app.js
+++ course-js_advanced/practice/app.js
@@ -1,46 +1,24 @@
 'use strict'
 
-// const arr = [1, 2, 3]
-// arr
-// 	.map((a) => a * 2)
-// 	.filter((a) => a > 0)
-// 	.find((a) => a < 100)
+class Character {
+	#inventory = []
+	#health = 10
 
-class Wallet {
-	balance = 0
-
-	add(sum) {
-		this.balance += sum
-		return this
+	pickItem(item) {
+		this.#inventory.push(item)
 	}
 
-	remove(sum) {
-		this.balance -= sum
-		return this
+	receiveDamage(damage) {
+		this.#health -= damage
 	}
 }
 
-const wallet = new Wallet()
-const res = wallet.add(100).remove(10)
-console.log(res)
-
-class Builder {
-	house = {}
-
-	addRoof() {
-		this.house.roof = 'Roof'
-		return this
+class DB {
+	save() {
+		localStorage.setItem('char', this)
 	}
 
-	addFloor() {
-		this.house.floor = 'Floor'
-		return this
-	}
-
-	execute() {
-		return this.house
+	loadCharacter() {
+		///....
 	}
 }
-
-const res2 = new Builder().addFloor().addRoof().execute()
-console.log(res2)
