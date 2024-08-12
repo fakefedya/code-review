030f81e8a3398430563b95bafd7222ff26c71cb8
Фёдор Полевой <polevoy@ligat.su>
2024-08-12 15:30:33 +0300

    10.4

--- course-js_advanced/practice/app.js
+++ course-js_advanced/practice/app.js
@@ -1,24 +1,27 @@
 'use strict'
 
-class Treasure {
-	value = 0
-}
+/*
+	Основная Идея: Принцип Барбары Лисков (LSP - Liskov Substitution Principle) указывает на то, что объекты класса-потомка должны быть способны заменять объекты базового класса без нарушения работоспособности программы.
+*/
 
-class Coin extends Treasure {
-	value = 1
-}
-
-class Crystal extends Treasure {
-	value = 10
-}
-
-class Brilliant extends Treasure {
-	value = 20
-}
-
-class Inventory {
-	#score
-	pick(treasure) {
-		this.#score += treasure.value
+class User {
+	#role = 'user'
+	getRole() {
+		return this.#role
 	}
 }
+
+class Admin extends User {
+	#role = ['user', 'admin']
+
+	getRole() {
+		return this.#role.join(', ')
+	}
+}
+
+function logRole(user) {
+	console.log('Role: ' + user.getRole().toUpperCase())
+}
+
+logRole(new User())
+logRole(new Admin())
