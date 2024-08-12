1366b084047cb5d7a07e0b2aa55214460f924324
Фёдор Полевой <polevoy@ligat.su>
2024-08-12 15:24:35 +0300

    10.3

--- course-js_advanced/practice/app.js
+++ course-js_advanced/practice/app.js
@@ -1,24 +1,24 @@
 'use strict'
 
-class Character {
-	#inventory = []
-	#health = 10
-
-	pickItem(item) {
-		this.#inventory.push(item)
-	}
-
-	receiveDamage(damage) {
-		this.#health -= damage
-	}
+class Treasure {
+	value = 0
 }
 
-class DB {
-	save() {
-		localStorage.setItem('char', this)
-	}
+class Coin extends Treasure {
+	value = 1
+}
 
-	loadCharacter() {
-		///....
+class Crystal extends Treasure {
+	value = 10
+}
+
+class Brilliant extends Treasure {
+	value = 20
+}
+
+class Inventory {
+	#score
+	pick(treasure) {
+		this.#score += treasure.value
 	}
 }
