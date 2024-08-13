689e84be243ef106291dca6ce80f387e89053cbf
Фёдор Полевой <polevoy@ligat.su>
2024-08-13 09:30:38 +0300

    11.6

--- course-js_advanced/practice/app.js
+++ course-js_advanced/practice/app.js
@@ -1,16 +1,17 @@
 'use strict'
 
-function req(id) {
-	const request = new XMLHttpRequest()
-	request.open('GET', 'https://dummyjson.com/products/' + id)
-	request.send()
+/*
+	Получить среднюю цену 30 товаров из API
+	https://dummyjson.com/products
+*/
 
-	request.addEventListener('load', function () {
-		const data = JSON.parse(this.responseText)
-		console.log(data)
-	})
-}
+const request = new XMLHttpRequest()
+request.open('GET', 'https://dummyjson.com/products')
+request.send()
 
-req(1)
-req('')
-req(3)
+request.addEventListener('load', function () {
+	const { products } = JSON.parse(this.responseText)
+	const sum = products.reduce((acc, p) => (acc += p.price), 0)
+	const avg = sum / products.length
+	console.log(avg)
+})
