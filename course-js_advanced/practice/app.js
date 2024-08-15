b11dd7f6b0ecba8b4fa2bf3300fd69b3923703c5
Фёдор Полевой <polevoy@ligat.su>
2024-08-15 10:03:08 +0300

    12.5

--- course-js_advanced/practice/app.js
+++ course-js_advanced/practice/app.js
@@ -1,11 +1,21 @@
 'use strict'
 
 const res = fetch('https://dummyjson.com/products')
-	.then((response) => response.json())
-	.then(({ products }) => {
-		return fetch('https://dummyjson.com/products/' + products[0].id)
-	})
+	.then(
+		(response) => response.json(),
+		(error) => console.log(error)
+	)
+	.then(
+		({ products }) => {
+			return fetch('https://dummyjson.com/products/' + products[0].id)
+		},
+		(error) => console.log(error)
+	)
 	.then((response) => response.json())
 	.then((data) => {
 		console.log(data)
+	})
+	.catch((error) => console.log(error))
+	.finally(() => {
+		console.log('Finally')
 	})
