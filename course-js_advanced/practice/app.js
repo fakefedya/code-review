b37da689d91f49d2325a98f28f1ebe9601135a17
Фёдор Полевой <polevoy@ligat.su>
2024-08-16 08:59:08 +0300

    12.6

--- course-js_advanced/practice/app.js
+++ course-js_advanced/practice/app.js
@@ -7,12 +7,19 @@
 
 const URL = 'https://dummyjson.com/products/category-list'
 
-function createSelect(options) {}
+function createSelect(options) {
+	document.body.innerHTML = `<select>
+		${options.map(
+			(optionsEl) => `<option value=${optionsEl}>${optionsEl}</option>`
+		)}
+	</select>`
+}
 
 function getCategories() {
 	fetch(URL)
 		.then((response) => response.json())
-		.then((data) => console.log(data))
+		.then((data) => createSelect(data))
+		.catch((error) => console.error(`Ошибка: ${error}`))
 }
 
 getCategories()
