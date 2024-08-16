569d6d35241c60d78837c3a37d02b29842b5b5bd
Фёдор Полевой <polevoy@ligat.su>
2024-08-16 09:27:28 +0300

    12.8

--- course-js_advanced/practice/app.js
+++ course-js_advanced/practice/app.js
@@ -1,20 +1,22 @@
 'use strict'
 
-const res = fetch('https://dummyjson.com/productss')
-	.then((response) => {
+function getData(url, errorMessage, method = 'GET') {
+	return fetch(url, {
+		method,
+	}).then((response) => {
 		if (!response.ok) {
-			throw new Error(`Is error: ${response.status}`)
+			throw new Error(`${errorMessage}: ${response.status}`)
 		}
 		return response.json()
 	})
+}
+
+getData('https://dummyjson.com/products', 'Can not get products')
 	.then(({ products }) => {
-		return fetch('https://dummyjson.com/products/' + products[0].id)
-	})
-	.then((response) => {
-		if (!response.ok) {
-			throw new Error(`Is error: ${response.status}`)
-		}
-		return response.json()
+		return getData(
+			'https://dummyjson.com/products/' + products[0].id,
+			'Can not get product 1'
+		)
 	})
 	.then((data) => {
 		console.log(data)
