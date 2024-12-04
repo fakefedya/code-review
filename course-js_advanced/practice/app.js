fac941c4460db7ed07ed6469930eef707823a9bd
Фёдор Полевой <polevoy@ligat.su>
2024-12-04 09:00:35 +0300

    14.6 Promise all

--- course-js_advanced/practice/app.js
+++ course-js_advanced/practice/app.js
@@ -1,33 +1,26 @@
 'use strict'
-function getCoordinates() {
-	return new Promise((res, rej) => {
-		navigator.geolocation.getCurrentPosition(
-			({ coords }) => {
-				res({ latitude: coords.latitude, longitude: coords.longitude })
-				console.log(res)
-			},
-			(err) => {
-				rej(err)
-			}
-		)
-	})
+
+async function getAllProducts() {
+	const response = await fetch('https://dummyjson.com/products')
+	return response.json()
 }
 
-async function getMyCity() {
-	try {
-		const { latitude, longitude } = await getCoordinates()
-
-		const response = await fetch(
-			`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`
-		)
-		if (!response.ok) {
-			throw new Error(response.status)
-		}
-		const data = await response.json()
-		console.log(data.city)
-	} catch (e) {
-		console.error(e)
-	}
+async function getProduct(id) {
+	const response = await fetch('https://dummyjson.com/products/' + id)
+	return response.json()
 }
 
-getMyCity()
+async function getProductError(id) {
+	const response = await fetch('https://dummyjsons.com/products/' + id)
+	return response.json()
+}
+
+async function main() {
+	const { products } = await getAllProducts()
+	const res = await Promise.all(
+		products.map((product) => getProduct(product.id))
+	)
+	console.log(res)
+}
+
+main()
