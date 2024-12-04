22337f1694409e0a2ae9edf5f7ec9142dd3497e6
Фёдор Полевой <polevoy@ligat.su>
2024-12-04 11:00:14 +0300

    14.9

--- course-js_advanced/practice/app.js
+++ course-js_advanced/practice/app.js
@@ -1,18 +1,30 @@
 'use strict'
 
-async function main() {
-	const res = await fetch('https://dummyjson.com/auth/login', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		body: JSON.stringify({
-			username: 'emilys',
-			password: 'emilyspass',
-		}),
-	})
-	const data = await res.json()
-	console.log(data)
+async function getRandomFact() {
+	const URL = 'https://uselessfacts.jsph.pl/api/v2/facts/random'
+	const response = await fetch(URL)
+	return response.json()
 }
 
-main()
+async function main() {
+	try {
+		const cardsWrapper = document.querySelector('.cards')
+		cardsWrapper.style.display = 'flex'
+		cardsWrapper.style.gap = '16px'
+		cardsWrapper.innerHTML = ``
+		const res = await Promise.all([
+			getRandomFact(),
+			getRandomFact(),
+			getRandomFact(),
+		])
+		for (const el of res) {
+			const card = document.createElement('div')
+			card.style.color = '#fff'
+			card.style.border = '1px solid red'
+			card.innerHTML = el.text
+			cardsWrapper.appendChild(card)
+		}
+	} catch (e) {
+		console.error(e)
+	}
+}
