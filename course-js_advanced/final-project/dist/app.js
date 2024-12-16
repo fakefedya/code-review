d2661a69e1f7450010fbf091ce0aa3e4b710c1ec
Fedor Polevoy <fedya@mbp16.local>
2024-12-16 20:21:18 +0300

    18.10. Упражнение - Состояние загрузки

--- course-js_advanced/final-project/dist/app.js
+++ course-js_advanced/final-project/dist/app.js
@@ -1151,6 +1151,11 @@
 			this.state = state;
 		}
 
+		search() {
+			const value = this.el.querySelector('input').value;
+			this.state.searchQuery = value;
+		}
+
 		render() {
 			this.el.classList.add('search');
 			this.el.innerHTML = `
@@ -1165,6 +1170,14 @@
            <img src="/static/search-white.svg" alt="Иконка поиска"/>
         </button>
     `;
+			this.el
+				.querySelector('button')
+				.addEventListener('click', this.search.bind(this));
+			this.el.querySelector('input').addEventListener('keydown', (event) => {
+				if (event.code == 'Enter') {
+					this.search();
+				}
+			});
 			return this.el
 		}
 	}
@@ -1181,6 +1194,7 @@
 			super();
 			this.appState = appState;
 			this.appState = onChange(this.appState, this.appStateHook.bind(this));
+			this.state = onChange(this.state, this.stateHook.bind(this));
 			this.setTitle('Поиск книг');
 		}
 
@@ -1188,6 +1202,26 @@
 			if (path === 'favorites') {
 				console.log(path);
 			}
+		}
+
+		async stateHook(path) {
+			if (path === 'searchQuery') {
+				this.state.loading = true;
+				const data = await this.loadList(
+					this.state.searchQuery,
+					this.state.offset
+				);
+				this.state.loading = false;
+				console.log(data);
+				this.state.list = data.docs;
+			}
+		}
+
+		async loadList(q, offset) {
+			const res = await fetch(
+				`https://openlibrary.org/search.json?q=${q}&offset=${offset}`
+			);
+			return res.json()
 		}
 
 		render() {
