40a2241531bc590b49daed40293badd28f9bcb66
Фёдор Полевой <polevoy@ligat.su>
2024-12-19 10:38:07 +0300

    4.3. Literal Types

--- course-typescript/lessons/app.ts
+++ course-typescript/lessons/app.ts
@@ -1,32 +1,7 @@
-function logId(id: string | number | boolean) {
-	if (typeof id === 'string') {
-		console.log(id.toLocaleLowerCase())
-	} else if (typeof id === 'number') {
-		console.log(id)
-	} else {
-		console.log(id)
-	}
+function fetchWithAuth(url: string, method: 'post' | 'get'): 1 | -1 {
+	return 1
 }
+fetchWithAuth('s', 'post')
 
-function logError(err: string | string[]) {
-	if (Array.isArray(err)) {
-		console.log(err)
-	} else {
-		console.log(err)
-	}
-}
-
-function logObject(obj: { a: number } | { b: number }) {
-	if ('a' in obj) {
-		console.log(obj.a)
-	} else {
-		console.log(obj.b)
-	}
-}
-
-function logMultipleIds(a: string | number, b: string | boolean) {
-	if (a === b) {
-	} else {
-		console.log(a)
-	}
-}
+let method = 'post'
+fetchWithAuth('s', method as 'post')
