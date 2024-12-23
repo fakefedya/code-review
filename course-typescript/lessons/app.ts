81c6b1818933be74fd77eb467adc66437b403e3e
Fedor Polevoy <fedya@mbp16.local>
2024-12-23 19:52:35 +0300

    5.8. Extends

--- course-typescript/lessons/app.ts
+++ course-typescript/lessons/app.ts
@@ -1,34 +1,35 @@
-enum PaymentStatus {
-	Holded,
-	Processed,
-	Reversed,
-}
+type PaymentStatus = 'new' | 'paid'
 
 class Payment {
 	id: number
-	status: PaymentStatus = PaymentStatus.Holded
-	createdAt: Date = new Date()
-	updatedAt: Date
+	status: PaymentStatus = 'new'
 
 	constructor(id: number) {
 		this.id = id
 	}
 
-	getPaymentLifeTime(): number {
-		return new Date().getTime() - this.createdAt.getTime()
-	}
-
-	unholdPayment(): void {
-		if (this.status === PaymentStatus.Processed) {
-			throw new Error('Платеж не может быть возвращен')
-		}
-		this.status = PaymentStatus.Reversed
-		this.updatedAt = new Date()
+	pay() {
+		this.status = 'paid'
 	}
 }
 
-const payment = new Payment(1)
-payment.unholdPayment()
-console.log(payment)
-const time = payment.getPaymentLifeTime()
-console.log(time)
+class PersistedPayment extends Payment {
+	databaseId: number
+	paidAt: Date
+
+	constructor() {
+		const id = Math.random()
+		super(id) //super() обязателен к вызову при переопределении
+	}
+
+	save() {
+		// Сохраняет в базу
+	}
+
+	override pay(date?: Date) {
+		super.pay()
+		if (date) {
+			this.paidAt = date
+		}
+	}
+}
