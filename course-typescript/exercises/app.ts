1a1307e89abf873aeefed3e5f17008f44ebbba08
Фёдор Полевой <polevoy@ligat.su>
2024-12-24 16:36:29 +0300

    5.12. Упражнение - Делаем корзину товаров

--- course-typescript/exercises/app.ts
+++ course-typescript/exercises/app.ts
@@ -1,21 +1,67 @@
-class User {
-	skills: string[] = []
+class Product {
+	constructor(public id: number, public title: string, public price: number) {}
+}
 
-	addSkill(skill: string): void
-	addSkill(skill: string[]): void
-	addSkill(skillOrSkills: string | string[]): void {
-		if (typeof skillOrSkills == 'string') {
-			this.skills.push(skillOrSkills)
-		} else if (Array.isArray(skillOrSkills)) {
-			this.skills = this.skills.concat(skillOrSkills)
-		} else {
-			throw new Error('Передано некорректное умение!')
-		}
+class Delivery {
+	constructor(public date: Date) {}
+}
+
+class HomeDelivery extends Delivery {
+	constructor(date: Date, public address: string) {
+		super(date)
 	}
 }
 
-const user = new User()
-user.addSkill('Плавать')
-user.addSkill('Летать')
-user.addSkill(['Бегать', 'Прыгать'])
-console.log(user.skills)
+type DeliveryOptions = HomeDelivery | ShopDelivery
+
+class ShopDelivery extends Delivery {
+	constructor(public shopId: number) {
+		super(new Date())
+	}
+}
+
+class Cart {
+	private products: Product[] = []
+	private delivery: DeliveryOptions
+
+	public addProduct(product: Product): void {
+		this.products.push(product)
+	}
+
+	public deleteProduct(productId: number): void {
+		this.products = this.products.filter((el: Product) => el.id !== productId)
+	}
+
+	public getSum(): number {
+		return this.products
+			.map((p: Product) => p.price)
+			.reduce((price_1: number, price_2: number) => price_1 + price_2)
+	}
+
+	public setDelivery(delivery: DeliveryOptions): void {
+		this.delivery = delivery
+	}
+
+	public checkOut() {
+		if (this.products.length == 0) {
+			throw new Error('Нет товаров в корзине')
+		}
+		if (!this.delivery) {
+			throw new Error('Нет указан способ доставки')
+		}
+		return { success: true }
+	}
+}
+
+const cart = new Cart()
+cart.addProduct(new Product(1, 'Хлеб', 75))
+cart.addProduct(new Product(2, 'Молоко', 128))
+cart.addProduct(new Product(3, 'Шоколад', 20))
+
+cart.deleteProduct(2)
+
+cart.setDelivery(new HomeDelivery(new Date(), ''))
+
+console.log(cart)
+console.log(cart.getSum())
+console.log(cart.checkOut())
