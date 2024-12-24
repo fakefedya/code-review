20e21d28e491390517fbf8ba5285360b43d95c60
Фёдор Полевой <polevoy@ligat.su>
2024-12-24 11:36:37 +0300

    5.11. Видимость свойств

--- course-typescript/lessons/app.ts
+++ course-typescript/lessons/app.ts
@@ -1,35 +1,33 @@
-type PaymentStatus = 'new' | 'paid'
+class Vehicle {
+	make: string //Публичное свойство
+	private damages: string[]
+	private _model: string
+	protected run: number
+	#price: number // Лучше не использовать в TS
 
-class Payment {
-	id: number
-	status: PaymentStatus = 'new'
-
-	constructor(id: number) {
-		this.id = id
+	set model(model: string) {
+		this._model = model
 	}
 
-	pay() {
-		this.status = 'paid'
+	get model() {
+		return this._model
+	}
+
+	isPriceEqual(v: Vehicle) {
+		return this.#price === v.#price
+	}
+
+	addDamage(damage: string) {
+		this.damages.push(damage)
+	}
+}
+
+class EuroTrack extends Vehicle {
+	setRun(km: number) {
+		this.run = km / 0.62
+		// this.damage - error
 	}
 }
 
-class PersistedPayment extends Payment {
-	databaseId: number
-	paidAt: Date
-
-	constructor() {
-		const id = Math.random()
-		super(id) //super() обязателен к вызову при переопределении
-	}
-
-	save() {
-		// Сохраняет в базу
-	}
-
-	override pay(date?: Date) {
-		super.pay()
-		if (date) {
-			this.paidAt = date
-		}
-	}
-}
+new Vehicle()
+new EuroTrack()
