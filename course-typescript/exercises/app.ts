413b7185fd9cb4fb00e900a778a3644296b87b48
Фёдор Полевой <polevoy@ligat.su>
2024-12-25 10:45:58 +0300

    5.17. Упражнение - Абстрактный logger

--- course-typescript/exercises/app.ts
+++ course-typescript/exercises/app.ts
@@ -1,67 +1,20 @@
-class Product {
-	constructor(public id: number, public title: string, public price: number) {}
-}
+abstract class Logger {
+	abstract log(message: string): void
 
-class Delivery {
-	constructor(public date: Date) {}
-}
-
-class HomeDelivery extends Delivery {
-	constructor(date: Date, public address: string) {
-		super(date)
+	printDate(date: Date): void {
+		this.log(date.toString())
 	}
 }
 
-type DeliveryOptions = HomeDelivery | ShopDelivery
-
-class ShopDelivery extends Delivery {
-	constructor(public shopId: number) {
-		super(new Date())
+class MyLogger extends Logger {
+	log(message: string): void {
+		console.log(message)
+	}
+	logWithDate(message: string): void {
+		this.printDate(new Date())
+		this.log(message)
 	}
 }
 
-class Cart {
-	private products: Product[] = []
-	private delivery: DeliveryOptions
-
-	public addProduct(product: Product): void {
-		this.products.push(product)
-	}
-
-	public deleteProduct(productId: number): void {
-		this.products = this.products.filter((el: Product) => el.id !== productId)
-	}
-
-	public getSum(): number {
-		return this.products
-			.map((p: Product) => p.price)
-			.reduce((price_1: number, price_2: number) => price_1 + price_2)
-	}
-
-	public setDelivery(delivery: DeliveryOptions): void {
-		this.delivery = delivery
-	}
-
-	public checkOut() {
-		if (this.products.length == 0) {
-			throw new Error('Нет товаров в корзине')
-		}
-		if (!this.delivery) {
-			throw new Error('Нет указан способ доставки')
-		}
-		return { success: true }
-	}
-}
-
-const cart = new Cart()
-cart.addProduct(new Product(1, 'Хлеб', 75))
-cart.addProduct(new Product(2, 'Молоко', 128))
-cart.addProduct(new Product(3, 'Шоколад', 20))
-
-cart.deleteProduct(2)
-
-cart.setDelivery(new HomeDelivery(new Date(), ''))
-
-console.log(cart)
-console.log(cart.getSum())
-console.log(cart.checkOut())
+const logger = new MyLogger()
+logger.logWithDate('Вот мое СМС')
