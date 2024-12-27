c30234ac701fae7f06eabb91c481ab40aee95d2a
Фёдор Полевой <polevoy@ligat.su>
2024-12-27 10:08:32 +0300

    7.5 Gererics

--- course-typescript/exercises/app.ts
+++ course-typescript/exercises/app.ts
@@ -1,20 +1,28 @@
-abstract class Logger {
-	abstract log(message: string): void
+/*
+	Написать универсальную обобщенную функцию toString, которая будет принимать разные типы данных и переводить их в строковое представление.
+*/
 
-	printDate(date: Date): void {
-		this.log(date.toString())
+function toString<T>(data: T): string | undefined {
+	if (Array.isArray(data)) {
+		return data.toString()
+	}
+	switch (typeof data) {
+		case 'string':
+			return data
+		case 'number':
+		case 'symbol':
+		case 'bigint':
+		case 'boolean':
+		case 'function':
+			return data.toString()
+		case 'object':
+			return JSON.stringify(data)
+		default:
+			return undefined
 	}
 }
 
-class MyLogger extends Logger {
-	log(message: string): void {
-		console.log(message)
-	}
-	logWithDate(message: string): void {
-		this.printDate(new Date())
-		this.log(message)
-	}
-}
-
-const logger = new MyLogger()
-logger.logWithDate('Вот мое СМС')
+console.log(toString(3))
+console.log(toString(true))
+console.log(toString(['test', 'test1']))
+console.log(toString({ name: 'Федя' }))
--- course-typescript/lessons/app.ts
+++ course-typescript/lessons/app.ts
@@ -1,19 +1,31 @@
-class UserService {
-	private static db: any
+function logMiddleware<T>(data: T): T {
+	console.log(data)
+	return data
+}
+const res = logMiddleware<number>(123)
 
-	static getUser(id: number) {
-		return UserService.db.findById(id)
-	}
-
-	create() {
-		UserService.db
-	}
-
-	static {
-		UserService.db = 'asd'
-	}
+function getHalfOfArr<T>(data: Array<T>): Array<T> {
+	const l = data.length / 2
+	return data.splice(0, l)
 }
 
-UserService.getUser(1)
-const inst = new UserService()
-inst.create()
+getHalfOfArr([1, 2, 3, 4])
+
+const split: <T>(data: Array<T>) => Array<T> = getHalfOfArr
+
+interface ILogLine<T> {
+	timeStamp: Date
+	data: T
+}
+
+type LogLineType<T> = {
+	timeStamp: Date
+	data: T
+}
+
+const logLine: ILogLine<{ a: number }> = {
+	timeStamp: new Date(),
+	data: {
+		a: 1,
+	},
+}
