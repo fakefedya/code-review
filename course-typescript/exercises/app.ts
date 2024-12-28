6dfbd5978b715962a9d6f84a79017ae39188be77
Фёдор Полевой <polevoy@ligat.su>
2024-12-28 10:14:01 +0300

    7.7. Упражнение - Функция сортировки id

--- course-typescript/exercises/app.ts
+++ course-typescript/exercises/app.ts
@@ -1,28 +1,41 @@
 /*
-	Написать универсальную обобщенную функцию toString, которая будет принимать разные типы данных и переводить их в строковое представление.
+	Необходимо написать функцию сортировки любых объектов, которые имеют id по убыванию и по возрастанию
 */
 
-function toString<T>(data: T): string | undefined {
-	if (Array.isArray(data)) {
-		return data.toString()
-	}
-	switch (typeof data) {
-		case 'string':
-			return data
-		case 'number':
-		case 'symbol':
-		case 'bigint':
-		case 'boolean':
-		case 'function':
-			return data.toString()
-		case 'object':
-			return JSON.stringify(data)
-		default:
-			return undefined
+const data = [
+	{ id: 2, name: 'Петя' },
+	{ id: 1, name: 'Вася' },
+	{ id: 3, name: 'Надя' },
+]
+
+interface ID {
+	id: number
+}
+
+function sort<T extends ID>(data: T[], type: string = 'asc'): T[] {
+	if (type == 'asc') {
+		return data.sort((a, b) => a.id - b.id)
+	} else if (type == 'desc') {
+		return data.sort((a, b) => b.id - a.id)
+	} else {
+		throw new Error('Параметр задан неверно')
 	}
 }
 
-console.log(toString(3))
-console.log(toString(true))
-console.log(toString(['test', 'test1']))
-console.log(toString({ name: 'Федя' }))
+console.log(sort(data, 'desc'))
+console.log(sort(data))
+console.log(sort(data, 'asd'))
+
+function alterSort<T extends ID>(data: T[], type: 'asc' | 'desc' = 'asc'): T[] {
+	return data.sort((a, b) => {
+		switch (type) {
+			case 'asc':
+				return a.id - b.id
+			case 'desc':
+				return b.id - a.id
+		}
+	})
+}
+
+console.log(alterSort(data))
+console.log(alterSort(data, 'desc'))
