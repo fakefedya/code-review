aa2a4491b6c0ccbdaccd06fb14f0ba56be598491
Фёдор Полевой <polevoy@ligat.su>
2025-01-10 09:47:21 +0300

    8.3. Упражнение - Пишем функцию группировки

--- course-typescript/exercises/app.ts
+++ course-typescript/exercises/app.ts
@@ -1,41 +1,38 @@
-/*
-	Необходимо написать функцию сортировки любых объектов, которые имеют id по убыванию и по возрастанию
-*/
+//8.3. Упражнение - Пишем функцию группировки
 
-const data = [
-	{ id: 2, name: 'Петя' },
-	{ id: 1, name: 'Вася' },
-	{ id: 3, name: 'Надя' },
+interface IData {
+	group: number
+	name: string
+}
+
+const data: IData[] = [
+	{ group: 1, name: 'Вася' },
+	{ group: 2, name: 'Федя' },
+	{ group: 1, name: 'Паша' },
 ]
 
-interface ID {
-	id: number
+type key = string | number | symbol
+
+interface IGroup<T> {
+	[key: key]: T[]
 }
 
-function sort<T extends ID>(data: T[], type: string = 'asc'): T[] {
-	if (type == 'asc') {
-		return data.sort((a, b) => a.id - b.id)
-	} else if (type == 'desc') {
-		return data.sort((a, b) => b.id - a.id)
-	} else {
-		throw new Error('Параметр задан неверно')
-	}
-}
-
-console.log(sort(data, 'desc'))
-console.log(sort(data))
-console.log(sort(data, 'asd'))
-
-function alterSort<T extends ID>(data: T[], type: 'asc' | 'desc' = 'asc'): T[] {
-	return data.sort((a, b) => {
-		switch (type) {
-			case 'asc':
-				return a.id - b.id
-			case 'desc':
-				return b.id - a.id
+function group<T extends Record<key, any>>(
+	array: T[],
+	key: keyof T
+): IGroup<T> {
+	return array.reduce<IGroup<T>>((map: IGroup<T>, item) => {
+		const itemKey = item[key]
+		let currentElement = map[itemKey]
+		if (Array.isArray(currentElement)) {
+			currentElement.push(item)
+		} else {
+			currentElement = [item]
 		}
-	})
+		map[itemKey] = currentElement
+		console.log(map)
+		return map
+	}, {})
 }
 
-console.log(alterSort(data))
-console.log(alterSort(data, 'desc'))
+group(data, 'group')
